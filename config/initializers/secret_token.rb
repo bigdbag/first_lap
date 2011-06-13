3a1e3178fe21f2ccf4e1f43f02f30ddbd2aee7ee
Sangduk Seo <sprintforyourlife@gmail.com>
2011-06-13 13:51:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstLap::Application.config.secret_token = 'f26501d85e9afc859199c6c300e830f6f33bb75f5f8745c04b1eb88ab4a2a250cfed9cc78ec0d897981b06d96f3f7a38737e90300853983d290a60d4ded61c98'
